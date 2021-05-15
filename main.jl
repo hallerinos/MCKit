802fd2d81f09b7770eec6b80b8869b8f02c41074
Andreas Haller <halleritis@gmail.com>
2021-05-15 18:46:40 +0200

    really the last one for today :D

--- main.jl
+++ main.jl
@@ -10,54 +10,51 @@ include("initialConfigurations.jl")
 include("generateParameters.jl")
 
 # system dimensions
-size = 11
+size = 101
 Lx = size
 Ly = size
 # coupling parameters
-D = 1.0
-𝐃₁ = [0,D,0]
-𝐃₂ = [D,0,0]
-J = -0.5*abs(D)
-# discretize the angles
-N∠ = 10000
+D = 1  # DMI amplitude
+𝐃₁ = [0,D,0]  # horizontal DMI
+𝐃₂ = [D,0,0]  # vertical DMI
+J = -1.0*abs(D)  # exchange coupling
+N∠ = 100  # discretize the angles
 Nϕ = 2*N∠  # ϕ ranges from [0,2π]
 Nθ = N∠  # θ ranges from [0,π]
-# inverse temperature (larger values reject ΔE>0 with high probability)
-β = 1e2
-# how many Metropolis steps per iteration?
-Nₛ = 1e5
-bc = "obc"
+β = 1e2  # inverse temperature
+Nₛ = 1e6  # how many Metropolis steps per iteration?
+bcs = ["obc"]  # boundary conditions
+Nₘ = 1000  # maximum number of iterations
+B₀s = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5].*(-abs(D))  # the magnetic fields which are simulated
 
-Nₘ = 100  # maximum number of iterations
-
-B₀s = [0.0,0.1, 0.2, 0.3, 0.4].*(-abs(D))  # the magnetic fields which are simulated
-
-for B₀ in B₀s, bc in ["obc"]  # loop over the fields and boundary conditions
+##
+for B₀ in B₀s, bc in bcs  # loop over the fields and boundary conditions
     fni = 0  # how many samples to simulate
     𝐁 = [0,0,B₀] 
     while fni<3
-        ps = generateParameters(Lx,Ly,J,𝐃₁,𝐃₂,𝐁,β,Nₛ,bc,Nϕ,Nθ,DMIHamiltonian)
-        configurations = initialConfigurations(ps)
+        ps = generateParameters(Lx,Ly,J,𝐃₁,𝐃₂,𝐁,β,Nₛ,bc,Nϕ,Nθ,DMIHamiltonian)  # simple Dict which contains everything
+        configurations = initialConfigurations(ps)  # draw random config
         # plot of the spin configurations
         # p = [heatmap(getindex.(configurations,i),title=L"S_%$i") for i=1:3]
         # display(plot(p...))
 
-        energy = []
+        energy = []  # collect the configuration energies
         iteration = 1
-        Nₐ = 2
-        append!(energy,calculateEnergy(configurations, ps))
+        append!(energy,calculateEnergy(configurations, ps))  # append the initial energy
         # perform the Metropolis algorithm
-        while Nₘ >= iteration && Nₐ > 1
-            configurations, ΔEs, Nₐ = metropolisAlgorithm(configurations, ps)
-            E̅ = sum(ΔEs)/Nₛ
-            append!(energy,calculateEnergy(configurations, ps))
-            Δε = (energy[end] - energy[end-1])/(Lx*Ly)
-            nₐ = Nₐ/Nₛ*100
-            @info "Running MC simulation for B=$B₀... iteration: " iteration E̅ Δε nₐ
+        while Nₘ >= iteration
+            # configurations, ΔEs, Nₐ = metropolisAlgorithm(configurations, ps)
+            # E̅ = sum(ΔEs)/Nₛ
+            metropolisAlgorithm!(configurations, ps)  # perform Nₛ MC steps
+            E = calculateEnergy(configurations, ps)  # calculate energy of new config
+            append!(energy,E)  # append energy
+            Δε = (energy[end] - energy[end-1])/(Lx*Ly)  # compute the energy drift
+            # nₐ = Nₐ/Nₛ*100  # step acceptance ratio
+            @info "Running MC simulation for B=$B₀... iteration: " iteration E Δε
             iteration += 1
         end
         p = [heatmap(getindex.(configurations,i),title=L"S_%$i") for i=1:3]
-        frac = Int64(floor(.5*iteration))
+        frac = Int64(floor(.9*iteration))  # show only a fraction of the energy history
         pE = plot(energy[end-frac:end]./(Lx*Ly),ylabel=L"\varepsilon_0",xlabel=L"N_s\times10^{%$(Int64(log10(Nₛ)))}",label=false,title="energy density")
         pf = plot(p..., pE, layout=4)
         # display(pf)
--- metropolisAlgorithm.jl
+++ metropolisAlgorithm.jl
@@ -88,3 +88,84 @@ function metropolisAlgorithm(configs,P)
 
     return cfgs, ΔEs, Nₐ
 end
+
+function metropolisAlgorithm!(cfgs,P)
+    Nϕ = Int64(P["Nϕ"])
+    Nθ = Int64(P["Nθ"])
+    H = P["H"]
+    β = P["β"]
+    J = P["J"]
+    𝐁 = P["𝐁"]
+    𝐃₁ = P["𝐃₁"]
+    𝐃₂ = P["𝐃₂"]
+    dϕs = P["dϕs"]
+    dθs = P["dθs"]
+
+    bc = P["bc"]
+
+    Lx = Int64(P["Lx"])
+    Ly = Int64(P["Ly"])
+
+    Nₛ = Int64(P["Nₛ"])
+
+    for s = 1:Nₛ
+        # draw the position
+        x = rand(1:Lx)
+        y = rand(1:Ly)
+        # draw the new angle
+        ϕ = dϕs[rand(1:Nϕ)]
+        θ = dθs[rand(1:Nθ)]
+        # copy the old configuration
+        oldS = cfgs[x,y]
+        newS = spinOrientation(ϕ,θ)
+        # compute the new and old local energies
+        newε = 0
+        oldε = 0
+        if x<Lx
+            newε += H(newS,cfgs[x+1,y],J,𝐃₁,𝐁)
+            oldε += H(oldS,cfgs[x+1,y],J,𝐃₁,𝐁)
+        end
+        if x>1
+            newε += H(cfgs[x-1,y],newS,J,𝐃₁,𝐁)
+            oldε += H(cfgs[x-1,y],oldS,J,𝐃₁,𝐁)
+        end
+        if y<Ly
+            newε += H(newS,cfgs[x,y+1],J,𝐃₂,𝐁)
+            oldε += H(oldS,cfgs[x,y+1],J,𝐃₂,𝐁)
+        end
+        if y>1
+            newε += H(cfgs[x,y-1],newS,J,𝐃₂,𝐁)
+            oldε += H(cfgs[x,y-1],oldS,J,𝐃₂,𝐁)
+        end
+        # consider also boundary contributions
+        if bc=="pbc"
+            if x==1
+                newε += H(cfgs[Lx,y],newS,J,𝐃₁,𝐁)
+                oldε += H(cfgs[Lx,y],oldS,J,𝐃₁,𝐁)
+            end
+            if x==Lx
+                newε += H(newS,cfgs[1,y],J,𝐃₁,𝐁)
+                oldε += H(oldS,cfgs[1,y],J,𝐃₁,𝐁)
+            end
+
+            if y==1
+                newε += H(cfgs[x,Ly],newS,J,𝐃₂,𝐁)
+                oldε += H(cfgs[x,Ly],oldS,J,𝐃₂,𝐁)
+            end
+            if y==Lx
+                newε += H(newS,cfgs[x,1],J,𝐃₂,𝐁)
+                oldε += H(oldS,cfgs[x,1],J,𝐃₂,𝐁)
+            end
+        end
+        # compute the energy difference
+        ΔE = newε-oldε
+        # acceptance probability
+        pₐ = min(1, exp(-β*ΔE))
+        # accept the new configuration if the acceptance probability pₐ is smaller than a random number
+        if rand()>pₐ
+            # @info "new configuration rejected"
+            newS = oldS
+        end
+        cfgs[x,y] = newS
+    end
+end
